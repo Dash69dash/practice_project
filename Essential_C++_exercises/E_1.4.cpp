e4f7fa1ee6a14112a2dca6c42c10b0188726c456
Dash69dash <972429608@qq.com>
2025-08-22 20:40:26 +0800

    1.3 done

---
+++ Essential_C++_exercises/E_1.4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <map>
+#include <fstream>
+using namespace std;
+int main() {
+    string user_name;
+    cout << "Enter your first name: ";
+    cin >> user_name;
+    cout << "\nHello, " << user_name << "!" << endl;
+    return 0;
+}
